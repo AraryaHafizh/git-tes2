9874e3de5ad2530a01487044d38623a9cc1dc723
AraryaHafizh <araryahafizh@gmail.com>
2023-03-25 09:49:34 +0700

    menambah function yang mengprint list

--- tes.py
+++ tes.py
@@ -1,2 +1,8 @@
 list_teman = ['andi', 'budi', 'carli', 'toni']
 list_guru = ['zani','mahendra','anto','lemon']
+
+def print_list(list):
+    for a in list:
+        print(a)
+
+print_list(list_teman)
